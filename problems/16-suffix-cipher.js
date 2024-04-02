322281b7ce39ade0c2d7f50efadbd268146d7fe7
Omega Dziko F. <megaoy@yahoo.com>
2024-04-02 18:09:40 -0400

    16th prb slvd

--- problems/16-suffix-cipher.js
+++ problems/16-suffix-cipher.js
@@ -32,7 +32,33 @@ console.log(suffixCipher('incremental progress is very instrumental', cipher2));
 *******************************************************************************/
 
 function suffixCipher(sentence, cipher) {
-  // Your code here 
+
+  //split sentence into arr
+  let sentArr = sentence.split(" ");
+
+  //run the map method on sentArr
+  let mapped = sentArr.map((word) => {
+
+    //loop through the obj
+    for (let suffix in cipher) {
+
+  //ask if the keys arr includes the end of the curr word
+        if(word.endsWith(suffix)) {
+
+  //if yes run the cb from the obj on curr word
+            return cipher[suffix](word);
+
+        }
+
+      }
+
+      return word;
+
+    });
+
+  //return mapped arr join into sentence
+  return mapped.join(" ");
+  
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
