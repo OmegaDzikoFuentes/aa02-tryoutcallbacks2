e3217bacea7fbb6545a664998883d1f1626fcfed
Omega Dziko F. <megaoy@yahoo.com>
2024-04-03 17:26:53 -0400

    22nd prb slvd

--- problems/22-first-index.js
+++ problems/22-first-index.js
@@ -23,7 +23,25 @@ console.log(result3); // -1
 *******************************************************************************/
 
 function firstIndex(array, cb) {
-  // Your code here  d
+
+  //loop through given arr
+  for (let i = 0; i < array.length; i++) {
+
+  //store value in variable
+    let val = array[i];
+
+  //ask if cb on value is true
+      if(cb(val)) {
+
+  //if true return index
+          return i;
+
+      }
+
+    }
+
+    return -1;
+
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
